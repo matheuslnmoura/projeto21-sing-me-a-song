b501db5d78f92fa618db6881eea8d03966288c5a
Matheus Moura <matheuslnmoura@gmail.com>
2022-07-28 15:19:06 -0300

    Feat: Create integration tests and install EsLint

--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -1,76 +1,181 @@
 import {faker} from '@faker-js/faker';
-import supertest from 'supertest'
+import supertest from 'supertest';
 
 
 import { prisma } from '../src/database.js';
-import app from './../src/app.js'
+import { recommendationRepository } from '../src/repositories/recommendationRepository.js';
+import app from './../src/app.js';
 import recommendationsFactory from './factories/recommendationsFactory.js';
 import scenarioFactory from './factories/scenarioFactory.js';
 
 beforeEach(async()=>{
-  await scenarioFactory.deleteAllData()
-})
+  await scenarioFactory.deleteAllData();
+});
 
 const agent = supertest(app);
 
 const recommendation = {
   name: faker.music.songName(),
   youtubeLink: 'https://www.youtube.com/watch?v=HLLuYxE-dgc'
-}
+};
 
 describe('create-recommendation', ()=>{
   it('Given valids inputs, should create a recommendation', async ()=>{
-    await agent.post('/recommendations').send(recommendation)
+    await agent.post('/recommendations').send(recommendation);
 
     const createdRecommendation = await prisma.recommendation.findFirst({
       where:{
         name: recommendation.name
       }
-    })
+    });
 
     expect(createdRecommendation).not.toBeNull();
-  })
+  });
 
   it('Given an empty name input and valid youtubeLink input, should return 422 code', async()=>{
     const recommendation = {
       name: '',
       youtubeLink: 'https://www.youtube.com/watch?v=HLLuYxE-dgc'
-    }
+    };
 
-    const response = await agent.post('/recommendations').send(recommendation)
+    const response = await agent.post('/recommendations').send(recommendation);
 
     expect(response.status).toBe(422);
-  })
+  });
 
   it('Given a valid name input and an empty youtubeLink input, should return 422 code', async()=>{
     const recommendation = {
       name: faker.music.songName(),
       youtubeLink: ''
-    }
+    };
 
-    const response = await agent.post('/recommendations').send(recommendation)
+    const response = await agent.post('/recommendations').send(recommendation);
 
     expect(response.status).toBe(422);
-  })
+  });
 
   it('Given a valid name input and an invalid Link, should return 422 code', async()=>{
     const recommendation = {
       name: faker.music.songName(),
       youtubeLink: faker.internet.url()
-    }
+    };
 
-    const response = await agent.post('/recommendations').send(recommendation)
+    const response = await agent.post('/recommendations').send(recommendation);
 
     expect(response.status).toBe(422);
-  })
-})
+  });
+});
+
+describe('/:id/upvote',()=>{
+  it('Given a valid id, should return upvotes number = 1', async()=>{
+    const {id} = await recommendationsFactory.insert(recommendation);
+
+    await agent.post(`/recommendations/${id}/upvote`);
+    const insertedRecommendation = await recommendationRepository.find(id);
+
+    expect(insertedRecommendation.score).toBe(1);
+  });
+
+  it('Given a valid id, should return upvotes number = 2 when recommendation has score number = 1', async()=>{
+    const {id} = await recommendationsFactory.insert(recommendation);
+
+    await agent.post(`/recommendations/${id}/upvote`);
+    await agent.post(`/recommendations/${id}/upvote`);
+    const insertedRecommendation = await recommendationRepository.find(id);
+
+    expect(insertedRecommendation.score).toBe(2);
+  });
+
+  it('Given a non-existing id, should return status 404', async()=>{
+    await recommendationsFactory.insert(recommendation);
+
+    const response = await agent.post('/recommendations/298/upvote');
+
+    expect(response.status).toBe(404);
+  });
+
+  it('Given an invalid id, should return status 500', async()=>{
+    await recommendationsFactory.insert(recommendation);
+
+    const response = await agent.post('/recommendations/banana/upvote');
+
+    expect(response.status).toBe(500);
+  });
+});
+
+describe('/:id/downvote',()=>{
+  it('Given a valid id, should return upvotes number = -1', async()=>{
+    const {id} = await recommendationsFactory.insert(recommendation);
+
+    await agent.post(`/recommendations/${id}/downvote`);
+    const insertedRecommendation = await recommendationRepository.find(id);
+
+    expect(insertedRecommendation.score).toBe(-1);
+  });
+
+  it('Given a valid id, should return upvotes number = 0 when recommendation has score number = 1', async()=>{
+    const {id} = await recommendationsFactory.insert(recommendation);
+
+    await agent.post(`/recommendations/${id}/upvote`);
+    await agent.post(`/recommendations/${id}/downvote`);
+    const insertedRecommendation = await recommendationRepository.find(id);
+
+    expect(insertedRecommendation.score).toBe(0);
+  });
+
+  it('Should remove a recommendation if score < -5', async()=>{
+    const {id} = await recommendationsFactory.insert(recommendation);
+
+    await recommendationsFactory.insertFiveDownVotes(id);
+
+    await agent.post(`/recommendations/${id}/downvote`);
+
+    const insertedRecommendation = await recommendationRepository.find(id);
+    console.log(insertedRecommendation);
+
+    expect(insertedRecommendation).toBeNull;
+  });
+
+  it('Given a non-existing id, should return status 404', async()=>{
+    await recommendationsFactory.insert(recommendation);
+
+    const response = await agent.post('/recommendations/298/upvote');
+
+    expect(response.status).toBe(404);
+  });
+
+  it('Given an invalid id, should return status 500', async()=>{
+    await recommendationsFactory.insert(recommendation);
+
+    const response = await agent.post('/recommendations/banana/upvote');
+
+    expect(response.status).toBe(500);
+  });
+});
 
 describe('get-recommendations',()=>{
-  it('Should return an array of recommendations', async()=>{
-    await recommendationsFactory.insert(recommendation)
+  it('Should return an array of recommendations with length = 1', async()=>{
+    await recommendationsFactory.insert(recommendation);
 
-    const recommendations = await agent.get('/recommendations')
+    const recommendations = await agent.get('/recommendations');
+    expect(recommendations.body.length).toBe(1);
+  });
 
-    expect(recommendations).not.toBeNull
-  })
-})
+  it('Should return an array of recommendations with length = 2', async()=>{
+    const recommendation2 = {
+      name: faker.music.songName(),
+      youtubeLink: 'https://www.youtube.com/watch?v=HLLuYxE-dgc'
+    };
+    await recommendationsFactory.insert(recommendation);
+    await recommendationsFactory.insert(recommendation2);
+
+    const recommendations = await agent.get('/recommendations');
+    expect(recommendations.body.length).toBe(2);
+  });
+
+  it('Considering 20 recommendations registred on Database, Should return an array of recommendations with length = 10', async()=>{
+    await recommendationsFactory.insertTwenty();
+    const recommendations = await agent.get('/recommendations');
+    expect(recommendations.body.length).toBe(10);
+  });
+});
--- back-end/tests/factories/recommendationsFactory.ts
+++ back-end/tests/factories/recommendationsFactory.ts
@@ -1,17 +1,47 @@
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
 
-import { prisma } from "../../src/database.js";
-import { CreateRecommendationData } from "../../src/services/recommendationsService.js";
+import { prisma } from '../../src/database.js';
+import { CreateRecommendationData } from '../../src/services/recommendationsService.js';
 
 async function insert(recommendation: CreateRecommendationData){
   return await prisma.recommendation.create({
     data: recommendation
-  })
+  });
+}
+
+async function insertTwenty() {
+  const INSERT_AMOUNT = 20;
+  const insertedRecommendations = [];
+  for (let i = 0; i < INSERT_AMOUNT; i++) {
+    console.log('entrou no for');
+    const recommendation = {
+      name: faker.music.songName(),
+      youtubeLink: 'https://www.youtube.com/watch?v=HLLuYxE-dgc'
+    };
+    const insertedRecommendation =  await prisma.recommendation.create({
+      data: recommendation
+    });
+    insertedRecommendations.push(insertedRecommendation);
+  }
+  return insertedRecommendations;
+}
+
+async function insertFiveDownVotes(id: number) {
+  return await prisma.recommendation.update({
+    where:{
+      id: id
+    },
+    data:{
+      score: -5
+    }
+  });
 }
 
 const recommendationsFactory = {
-  insert
-}
+  insert,
+  insertTwenty,
+  insertFiveDownVotes
+};
 
-export default recommendationsFactory
+export default recommendationsFactory;
